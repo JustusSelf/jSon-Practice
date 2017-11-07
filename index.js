e113b258e0fb1a4c7e65e30557a53652b3d04dcb
SELF <js0877658@otc.edu>
2017-11-07 16:23:56 -0600

    all the stuff

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("hello world");
+var _ = require('lodash');
+var json = require('jsonfile');
+const file = 'data.json';
+json.readFile(file, function(err, data) {
+    console.log("----------");
+    for (i = 0; i < data.items.length; i++) {
+        _.forEach(data.items[i], function(value) {
+            console.log(value);
+        });
+        console.log("----------");
+    };
+});
